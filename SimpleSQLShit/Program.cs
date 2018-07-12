dcd93279fc136b553b3f9acccc7bf5e2615d09c7
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-12 15:16:41 -0400

    Add project files.

---
+++ SimpleSQLShit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleSQLShit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
